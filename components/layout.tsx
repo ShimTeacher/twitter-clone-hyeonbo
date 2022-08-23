0373ad8fe555807cd6e304392a699f5fed538c6b
Hyeonbo <hyeonbo.dev@gmail.com>
2022-08-24 01:30:47 +0900

    Layout set completed

--- components/layout.tsx
+++ components/layout.tsx
@@ -24,168 +24,97 @@ export default function Layout({
     router.back();
   };
   return (
-    <div>
-      <Head>
-        <title>{seoTitle} | Carrot Market</title>
-      </Head>
-      <div className="bg-white w-full h-12 max-w-xl justify-center text-lg px-10 font-medium  fixed text-gray-800 border-b top-0  flex items-center">
-        {canGoBack ? (
-          <button onClick={onClick} className="absolute left-4">
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 19l-7-7 7-7"
-              ></path>
-            </svg>
-          </button>
-        ) : null}
-        {title ? (
-          <span className={cls(canGoBack ? "mx-auto" : "", "")}>{title}</span>
-        ) : null}
+    <div className="bg-black h-screen flex">
+    
+    <div className="border-r border-gray-800 flex flex-col gap-2  xl:w-60">
+      <svg
+        className="text-white w-14 h-14 p-4 cursor-pointer ml-2 mr-2"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 24 24">
+        <path
+          d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"
+        />
+      </svg>
+      <div className="flex justify-start items-center  hover:bg-[#181819] rounded-full cursor-pointer ml-2 mr-2">
+        <svg className="text-white w-14 h-14 p-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+        <span className="hidden text-white xl:block pr-6">홈</span>
       </div>
-      <div className={cls("pt-12", hasTabBar ? "pb-24" : "")}>{children}</div>
-      {hasTabBar ? (
-        <nav className="bg-white max-w-xl text-gray-700 border-t fixed bottom-0 w-full px-10 pb-5 pt-3 flex justify-between text-xs">
-          <Link href="/">
-            <a
-              className={cls(
-                "flex flex-col items-center space-y-2 ",
-                router.pathname === "/"
-                  ? "text-orange-500"
-                  : "hover:text-gray-500 transition-colors"
-              )}
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                ></path>
-              </svg>
-              <span>홈</span>
-            </a>
-          </Link>
-          <Link href="/community">
-            <a
-              className={cls(
-                "flex flex-col items-center space-y-2 ",
-                router.pathname === "/community"
-                  ? "text-orange-500"
-                  : "hover:text-gray-500 transition-colors"
-              )}
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
-                ></path>
-              </svg>
-              <span>동네생활</span>
-            </a>
-          </Link>
-          <Link href="/chats">
-            <a
-              className={cls(
-                "flex flex-col items-center space-y-2 ",
-                router.pathname === "/chats"
-                  ? "text-orange-500"
-                  : "hover:text-gray-500 transition-colors"
-              )}
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                ></path>
-              </svg>
-              <span>채팅</span>
-            </a>
-          </Link>
-          <Link href="/streams">
-            <a
-              className={cls(
-                "flex flex-col items-center space-y-2 ",
-                router.pathname === "/streams"
-                  ? "text-orange-500"
-                  : "hover:text-gray-500 transition-colors"
-              )}
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
-                ></path>
-              </svg>
-              <span>라이브</span>
-            </a>
-          </Link>
-          <Link href="/profile">
-            <a
-              className={cls(
-                "flex flex-col items-center space-y-2 ",
-                router.pathname === "/profile"
-                  ? "text-orange-500"
-                  : "hover:text-gray-500 transition-colors"
-              )}
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                ></path>
-              </svg>
-              <span>나의 캐럿</span>
-            </a>
-          </Link>
-        </nav>
-      ) : null}
+      <div className="flex justify-start items-center  hover:bg-[#181819] rounded-full cursor-pointer ml-2 mr-2">
+        <svg className="text-white w-14 h-14 p-4 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+        <span className="hidden text-white xl:block pr-6">탐색하기</span>
+      </div>
+      <div className="flex justify-start items-center  hover:bg-[#181819] rounded-full cursor-pointer ml-2 mr-2">
+        <svg className="text-white w-14 h-14 p-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
+        <span className="hidden text-white xl:block pr-6">알림</span>
+      </div>
+      <div className="flex justify-start items-center  hover:bg-[#181819] rounded-full cursor-pointer ml-2 mr-2">
+        <svg className="text-white w-14 h-14 p-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg>
+        <span className="hidden text-white xl:block pr-6">쪽지</span>
+      </div>
+      <div className="flex justify-start items-center  hover:bg-[#181819] rounded-full cursor-pointer ml-2 mr-2">
+        <svg className="text-white w-14 h-14 p-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" /></svg>
+        <span className="hidden text-white xl:block pr-6">북마크</span>
+      </div>
+      <div className="flex justify-start items-center  hover:bg-[#181819] rounded-full cursor-pointer ml-2 mr-2">
+        <svg className="text-white w-14 h-14 p-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" /></svg>
+        <span className="hidden text-white xl:block pr-6">리스트</span>
+      </div>
+      <div className="flex justify-start items-center  hover:bg-[#181819] rounded-full cursor-pointer ml-2 mr-2">
+        <svg className="text-white w-14 h-14 p-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>
+        <span className="hidden text-white xl:block pr-6">프로필</span>
+      </div>
+      <div className="flex justify-start items-center  hover:bg-[#181819] rounded-full cursor-pointer ml-2 mr-2">
+        <svg className="text-white w-14 h-14 p-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        <span className="hidden text-white xl:block pr-6">더보기</span>
+      </div>
+      <div className="flex justify-center items-center  rounded-full cursor-pointer ">
+        <svg className="text-white w-14 h-14 p-4  hover:bg-[#277cbd] rounded-full cursor-pointer bg-[#2D8CD7] xl:hidden" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M 8.8 7.2 H 5.6 V 3.9 c 0 -0.4 -0.3 -0.8 -0.8 -0.8 s -0.7 0.4 -0.7 0.8 v 3.3 H 0.8 c -0.4 0 -0.8 0.3 -0.8 0.8 s 0.3 0.8 0.8 0.8 h 3.3 v 3.3 c 0 0.4 0.3 0.8 0.8 0.8 s 0.8 -0.3 0.8 -0.8 V 8.7 H 9 c 0.4 0 0.8 -0.3 0.8 -0.8 s -0.5 -0.7 -1 -0.7 Z m 15 -4.9 v -0.1 h -0.1 c -0.1 0 -9.2 1.2 -14.4 11.7 c -3.8 7.6 -3.6 9.9 -3.3 9.9 c 0.3 0.1 3.4 -6.5 6.7 -9.2 c 5.2 -1.1 6.6 -3.6 6.6 -3.6 s -1.5 0.2 -2.1 0.2 c -0.8 0 -1.4 -0.2 -1.7 -0.3 c 1.3 -1.2 2.4 -1.5 3.5 -1.7 c 0.9 -0.2 1.8 -0.4 3 -1.2 c 2.2 -1.6 1.9 -5.5 1.8 -5.7 Z" /></svg>
+        <span className="hidden xl:block text-center justify-center text-white p-4 bg-[#2D8CD7] rounded-full w-full font-bold text-xl m-2 hover:bg-[#277cbd]">트윗하기</span>
+      </div>
+      <div className="flex flex-1 justify-center items-end">
+        <svg className="text-white w-14 h-14 p-4 hover:bg-[#181819] rounded-full cursor-pointer" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+      </div>
     </div>
+
+    <div className="flex-1 bg-black w-full flex flex-row gap-2 ">
+      <div className="flex-1 h-screen border-r border-gray-800 ">
+        {children}
+      </div>
+      <div className="hidden bg-[#17181C] lg:block w-96 p-5 rounded-xl m-3">
+        <div className="mb-10">
+          <span className="text-white text-2xl font-bold">나를 위한 트랜드</span>  
+        </div>
+
+        <div className="flex flex-col justify-center w-full mb-10">
+          <span className="text-gray-500 font-normal text-sm">대한민국에서 트렌드 중</span>
+          <span className="text-white font-bold">참치 초고추장</span>
+          <span className="text-gray-500 font-normal text-sm">9,142 트윗</span>
+          <svg className="fixed right-10 w-6 h-6 text-gray-500" fill="currentColor" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+        </div>
+
+        <div className="flex flex-col justify-center w-full mb-2">
+          <span className="text-gray-500 font-normal text-sm">대한민국에서 트렌드 중</span>
+          <span className="text-white font-bold">참치 초고추장</span>
+          <span className="text-gray-500 font-normal text-sm">9,142 트윗</span>
+          <svg className="fixed right-10 w-6 h-6 text-gray-500" fill="currentColor" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+        </div>
+
+        <div className="flex flex-col justify-center w-full mb-2">
+          <span className="text-gray-500 font-normal text-sm">대한민국에서 트렌드 중</span>
+          <span className="text-white font-bold">참치 초고추장</span>
+          <span className="text-gray-500 font-normal text-sm">9,142 트윗</span>
+          <svg className="fixed right-10 w-6 h-6 text-gray-500" fill="currentColor" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+        </div>
+        
+        <div className="flex flex-col justify-center w-full mb-2">
+          <span className="text-gray-500 font-normal text-sm">대한민국에서 트렌드 중</span>
+          <span className="text-white font-bold">참치 초고추장</span>
+          <span className="text-gray-500 font-normal text-sm">9,142 트윗</span>
+          <svg className="fixed right-10 w-6 h-6 text-gray-500" fill="currentColor" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+        </div>        
+      </div>
+    </div>
+  </div>
   );
 }
